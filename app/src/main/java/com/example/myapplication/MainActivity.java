fa25ed70e18781afbaaf0b8a84c057e08bd279d2
Vinícius <viniciusstorch62@gmail.com>
2025-08-25 08:12:48 -0300

    Reescrevendo métodos do ciclo de vida

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,12 +14,55 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        Log.d("ciclo_vida", "Oncreate");
+//        Log.w("", "");
+//        Log.i("", "");
+//        Log.wtf("", "");
+        }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("ciclo_vida", "Onstart");
+//        Log.w("", "");
+//        Log.i("", "");
+//        Log.wtf("", "");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("ciclo_vida", "Onresume");
+//        Log.w("", "");
+//        Log.i("", "");
+//        Log.wtf("", "");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("ciclo_vida", "Onpause");
+//        Log.w("", "");
+//        Log.i("", "");
+//        Log.wtf("", "");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+          Log.d("ciclo_vida", "Onstop");
+//        Log.w("", "");
+//        Log.i("", "");
+//        Log.wtf("", "");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("ciclo_vida", "Ondestroy");
+//        Log.w("", "");
+//        Log.i("", "");
+//        Log.wtf("", "");
     }
 }
