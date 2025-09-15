1499189fd837944152e0389ae7c2c5bfea828a99
Vinícius <viniciusstorch62@gmail.com>
2025-09-15 09:07:07 -0300

    Calculo do imc em ImcResultado

--- app/src/main/java/com/example/myapplication/ImcResultado.java
+++ app/src/main/java/com/example/myapplication/ImcResultado.java
@@ -1,6 +1,10 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,11 +14,28 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class ImcResultado extends AppCompatActivity {
 
+    Button b;
+    TextView tvResultado;
+    ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_imc_resultado);
+        tvResultado=findViewById(R.id.textView3);
+        imageView = findViewById(R.id.imageView2);
+
+        // Recuperando a intent
+        Intent i = getIntent();
+        Bundle b = i.getExtras();
+        Double peso = b.getDouble("peso");
+        Double altura = b.getDouble("altura");
+
+        // Calcular imc
+        Double imc = peso/(altura*altura);
+
+        tvResultado.setText(Double.toString(imc));
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
