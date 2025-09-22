fdf43327f3cf281ea6111604eac0c72f1d6abdcd
Vinícius <viniciusstorch62@gmail.com>
2025-09-22 08:37:52 -0300

    Redirecionamento para Activity2 concluído

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,11 +13,25 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button button;
+    EditText editText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.button);
+        editText = findViewById(R.id.editTextText);
+
+        button.setOnClickListener(v -> {
+            Intent i = new Intent(this, Activity2.class);
+            Bundle bundle = new Bundle();
+            Double n = Double.parseDouble(editText.getText().toString());
+            bundle.putDouble("n", n);
+            i.putExtras(bundle);
+            startActivity(i);
+        });
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
