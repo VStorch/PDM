a44a52199a70e6e491b2144ae3113b398d9bbdb1
Vinícius <viniciusstorch62@gmail.com>
2025-10-13 08:51:22 -0300

    Atividade da última semana, código copiado do repositório do professor com alguns ajustes

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,32 +1,59 @@
 package com.example.myapplication;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
+
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     ListView listView;
-
+    Button buttonSalvar;
+    EditText editText;
     ArrayList<String> nomes;
 
-    PlanetaController planetaController;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        listView = findViewById(R.id.idListView);
-        planetaController = new PlanetaController();
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
-                android.R.layout.simple_list_item_1, planetaController.getNomePlanetas());
-        listView.setAdapter(adapter);
+        listView =findViewById(R.id.listView); //Associando view a variavel local listView
+        buttonSalvar = findViewById(R.id.buttonSalvar); //Associando view a variavel local
+        editText = findViewById(R.id.editTextText); //Associando view a variavel local
+        nomes = new ArrayList<String>();//Inicializando lista de nomes com ArrayList Vazio
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, nomes);
+        listView.setAdapter(adapter);//Definindo adapter para a listView
+        buttonSalvar.setOnClickListener( v -> {
+            nomes.add(editText.getText().toString());
+            adapter.notifyDataSetChanged();
+        });
+        listView.setOnItemLongClickListener( (parent, view, position, id) -> {
+            AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+            builder.setTitle("Tem Certeza que quer excluir?")
+                    .setMessage("Confirma a excluisão do item!")
+                    .setPositiveButton("OK", (dialog, which) -> {
+                        nomes.remove(position);
+                        adapter.notifyDataSetChanged();
+                        Toast.makeText(MainActivity.this, "Item excluído com sucesso!", Toast.LENGTH_SHORT).show();
+                    })
+                    .setNegativeButton("Cancelar", (dialog, which) -> {
+                        Toast.makeText(MainActivity.this, "Operação cancelada!", Toast.LENGTH_SHORT).show();
+                    })
+                    .setCancelable(false); // Impede o fechamento clicando fora do diálogo
+            // Exibe o Diálogo
+            builder.show();
+
+            return true;
+        });
+
     }
 }
