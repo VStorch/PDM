fe8dff15ddbd96be64ae7ae836ca6c2900b5f202
Vinícius <viniciusstorch62@gmail.com>
2025-11-24 08:59:45 -0300

    Listagem de sensores do emulados implementada

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -6,14 +6,20 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
     SensorManager sm;
     TextView textView;
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,9 +27,18 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
         textView = findViewById(R.id.textViewSensor);
+        listView = findViewById(R.id.listView);
         sm = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         Sensor sensor = sm.getDefaultSensor(Sensor.TYPE_LIGHT);
         sm.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL);
+
+        List<Sensor> sensorList = sm.getSensorList(Sensor.TYPE_ALL);
+        ArrayList<String> listNameSensor = new ArrayList<>();
+        for (Sensor s: sensorList) {
+            listNameSensor.add(s.getName());
+        }
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, listNameSensor);
+        listView.setAdapter(adapter);
     }
 
     @Override
